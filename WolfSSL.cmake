a6a48b73a25d1a4fb5ebe351b388d7ffac4f9f3f
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2023-12-03 20:35:50 +0100

    Cleanup

--- WolfSSL.cmake
+++ WolfSSL.cmake
@@ -1,24 +1,32 @@
 
-# All WolfSSL configuration is done in the user_settings.h file
-set(WOLFSSL_USER_SETTINGS "yes")
+# Liboqs configuration
+set(OQS_DIST_BUILD  OFF)
+set(OQS_BUILD_ONLY_LIB ON)
+set(OQS_USE_OPENSSL OFF)
 
-# Build neither the examples nor the test stuff
-set(WOLFSSL_EXAMPLES "no")
-set(WOLFSSL_CRYPT_TESTS "no")
-set(BUILD_SHARED_LIBS "no")
-set(WOLFSSL_OQS "yes")
+# Add liboqs to the build environment
+add_subdirectory(libs/liboqs)
 
+# WolfSSL configuration
+set(WOLFSSL_USER_SETTINGS ON)
+set(WOLFSSL_EXAMPLES OFF)
+set(WOLFSSL_CRYPT_TESTS OFF)
+set(BUILD_SHARED_LIBS OFF)
+set(WOLFSSL_OQS OFF)
 
 # Add WolfSSL to the build environment
-add_subdirectory(libs/wolfssl wolfssl)
-
+add_subdirectory(libs/wolfssl)
 
 # In order for WolfSSL to find the 'user_settings.h' file, we have to
 # provide the directory where it is located.
 target_include_directories(wolfssl PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/config/wolfssl>)
 
+# Provide the liboqs headers (generated during build) for WolfSSL
+get_target_property(OQS_BINARY_DIR oqs BINARY_DIR)
+target_include_directories(wolfssl PUBLIC $<BUILD_INTERFACE:${OQS_BINARY_DIR}/../include>)
 
-# Add the library to the actual target
+# Link liboqs to WolfSSL
+target_link_libraries(wolfssl PRIVATE oqs)
+
+# Link WolfSSL to the main target
 target_link_libraries(${TARGET} PUBLIC wolfssl)
-
-

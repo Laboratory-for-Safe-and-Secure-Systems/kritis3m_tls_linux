c0e2b1604d3eaeea0d7c081069fdbea8355f59bc
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2024-05-13 09:27:51 +0200

    WolfSSL config cleanup

--- config/wolfssl/user_settings.h
+++ config/wolfssl/user_settings.h
@@ -5,7 +5,57 @@
 extern "C" {
 #endif
 
-// Disable unused features
+
+/* Feature support */
+#define WOLFSSL_TLS13
+#define WOLFSSL_USER_IO
+#define WOLF_CRYPTO_CB
+#define WOLF_CRYPTO_DEV
+#define WOLFSSL_KEY_GEN
+#define WOLFSSL_ALWAYS_VERIFY_CB
+#define HAVE_CRL_IO
+#define HAVE_TLS_EXTENSIONS
+#define HAVE_SUPPORTED_CURVES
+#define HAVE_NULL_CIPHER
+#define HAVE_SECRET_CALLBACK
+#define HAVE_WOLF_BIGINT
+#define HAVE_PKCS11
+#define WOLFSSL_DUAL_ALG_CERTS
+#define WOLFSSL_ASN_TEMPLATE
+#define LARGE_STATIC_BUFFERS
+
+#define NO_PWDBASED
+#define NO_WRITEV
+#define NO_DEV_URANDOM
+#define NO_MULTIBYTE_PRINT
+#define NO_OLD_TLS
+#define WOLFSSL_NO_TLS12
+#define NO_OLD_RNGNAME
+#define WOLFSSL_NO_SOCK
+#define NO_PKCS11_ECDH
+
+
+/* Enable experimental features */
+#define WOLFSSL_EXPERIMENTAL_SETTINGS
+
+
+/* Algorithm support and configuration */
+#define HAVE_ECC
+#define HAVE_ECC384
+#define ECC_SHAMIR
+#define ECC_TIMING_RESISTANT
+#define WC_RSA_PSS
+#define WC_RSA_BLINDING
+#define WOLFSSL_SHA384
+#define WOLFSSL_SHA512
+#define WOLFSSL_SHA3
+#define WOLFSSL_SHAKE128
+#define WOLFSSL_SHAKE256
+#define HAVE_HKDF
+#define HAVE_AESGCM
+#define HAVE_LIBOQS
+// #define WOLFSSL_WC_KYBER
+
 #define NO_SHA
 #define NO_MD4
 #define NO_MD5
@@ -13,52 +63,8 @@ extern "C" {
 #define NO_DSA
 #define NO_DH
 #define NO_RC4
-#define NO_PWDBASED
-// #define NO_SESSION_CACHE
-// #define NO_RESUME_SUITE_CHECK
-#define NO_WRITEV
-#define NO_DEV_URANDOM
-#define NO_MULTIBYTE_PRINT
-#define NO_OLD_TLS
-#define WOLFSSL_NO_TLS12
-#define NO_OLD_RNGNAME
 #define NO_AES_192
 
-// Enable features
-#define WOLFSSL_USER_IO
-#define HAVE_ECC384
-#define WOLF_CRYPTO_CB
-#define WOLF_CRYPTO_DEV
-#define WOLFSSL_NO_SOCK
-#define WOLFSSL_KEY_GEN
-#define WOLFSSL_ALWAYS_VERIFY_CB
-#define HAVE_CRL_IO
-#define HAVE_AESGCM
-#define WOLFSSL_TLS13
-#define HAVE_HKDF
-#define HAVE_FFDHE_4096
-#define WC_RSA_PSS
-#define WC_RSA_BLINDING
-#define WOLFSSL_SHA384
-#define WOLFSSL_SHA512
-#define HAVE_TLS_EXTENSIONS
-#define HAVE_SUPPORTED_CURVES
-#define HAVE_ECC
-// #define HAVE_SESSION_TICKET
-#define HAVE_NULL_CIPHER
-#define ECC_SHAMIR
-#define ECC_TIMING_RESISTANT
-#define HAVE_SECRET_CALLBACK
-
-#define HAVE_WOLF_BIGINT
-#define HAVE_PKCS11
-#define NO_PKCS11_ECDH
-
-#define HAVE_LIBOQS
-#define WOLFSSL_DUAL_ALG_CERTS
-#define WOLFSSL_ASN_TEMPLATE
-
-#define LARGE_STATIC_BUFFERS
 
 /* Debugging */
 #define DEBUG_WOLFSSL

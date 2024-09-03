3cbf610b32e99595398e1406a3f8e79e49c6bda7
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2024-09-03 09:54:10 +0200

    Add helper script for proxy based echo server

---
+++ scripts/tls_echo_server_proxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+	echo "Invalid argument count: needs at least 2 (<port> destination, <cert_dir>)" >&2
+	exit 2
+fi
+
+
+echo -e "Starting KRITIS3M TLS server with internal reverse proxy on port $1 with certs in dir $2\r"
+kritis3m_tls echo_server_proxy \
+	--incoming $1 \
+	--root $2/root/cert.pem \
+	--cert $2/server/chain.pem \
+	--key $2/server/privateKey.pem \
+	${@:3}

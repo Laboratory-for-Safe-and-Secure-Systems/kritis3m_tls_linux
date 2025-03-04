7bd7e5c8aba68d1a7dd42a2d2ca497752b69e472
Lukas Fuereder <lukas.fuereder@oth-regensburg.de>
2025-03-04 15:40:56 +0100

    removed keylog-file logic from the CLI parsing.

--- src/cli_parsing.c
+++ src/cli_parsing.c
@@ -618,12 +618,6 @@ static int check_qkd_config(asl_endpoint_configuration* qkd_config,
                         return -1;
                 }
 
-                if(tls_config->keylog_file != NULL)
-                {
-                        /* For debug reasons, we copy the keylog_file from the tls_config */
-                        qkd_config->keylog_file = duplicate_string(tls_config->keylog_file);
-                }
-
                 /* We pass the resulting asl_endpoint to the callback_ctx */
                 asl_endpoint* https_endpoint = asl_setup_client_endpoint(qkd_config);
                 tls_config->psk.callback_ctx = https_endpoint;
@@ -650,11 +644,6 @@ static int check_qkd_config(asl_endpoint_configuration* qkd_config,
         {
                 free((void*) qkd_config->device_certificate_chain.buffer);
                 qkd_config->device_certificate_chain.size = 0;
-        }
-        if (qkd_config->keylog_file != NULL)
-        {
-                free((void*) qkd_config->keylog_file);
-                qkd_config->keylog_file = NULL;
         }
 
         return 0;
